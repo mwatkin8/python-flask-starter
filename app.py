ff94f69113796f9ca3d1ea8c32ebb9b107d49d1d
Michael Watkins <michael.watkins.8@gmail.com>
2018-12-22 08:51:11 -1000

    added code documentation

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ import getFHIR
 
 app = Flask(__name__)
 
+'''
+The app route parameter specifies when each function in app.py will be called. Since this is our initial function we
+want called, we will designate it to run when users navigate to the base url of our app. In this example the base 
+will be http://0.0.0.0:8080/ but you could specify other routes such as http://0.0.0.0:8080/details or 
+http://0.0.0.0:8080/list to launch additional views for your app with each function rendering different templates.
+'''
 @app.route('/')
 def launch():
+    '''
+    Launches our initial view by rendering index.html and calling each method from our imported getFHIR.py file.
+    The return from each method will populate the dynamic variables designated by double curly braces in the HTML.
+    '''
     return render_template('index.html',
                            patient_example=getFHIR.getPatient(),
                            condition_example=getFHIR.getCondition())
 
+#Allows you to run this file with the command 'python app.py' and launch the Flask server on the designated host and port.
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
--- getFHIR.py
+++ getFHIR.py
@@ -2,6 +2,7 @@ from fhirclient import client
 import fhirclient.models.patient as p
 import fhirclient.models.condition as c
 
+#A dictionary used to initialize the FHIRClient settings and specify the FHIR server you want to query.
 settings = {
     'app_id': 'python_FHIR_app',
     'api_base': 'http://fhirtest.uhn.ca/baseDstu3/'
@@ -13,11 +14,12 @@ def getPatient():
     This method shows how to use the fhirclient's Patient model to get FHIR data from a server.
     This example returns the ID, name, and birth year for all females over 45 years old.
     '''
+    #Query to FHIR server for all female patients
     search = p.Patient.where(struct={'gender': 'female'})
     results = search.perform_resources(server)
     count = 0
     data = ''
-
+    #For each female Patient resource returned, extract the birth year to determine age
     for r in results:
         try:
             birthyear = int(r.as_json()['birthDate'].split('-')[0])
@@ -28,7 +30,7 @@ def getPatient():
                 count += 1
         except:
             continue
-
+    #Return this information to app.py to include in the HTML rendering
     return 'ID, name, and birth year for each female over 45 years old in the ' + settings['api_base'] + \
            ' FHIR server.\n\nTotal: ' + str(count) + '\n\n' + data
 
@@ -37,17 +39,18 @@ def getCondition():
     This method shows how to use both the fhirclient Condition and Patient models to get FHIR data from a server.
     This example returns the ID and name of each patient with hypertension.
     '''
+    #Query the FHIR server for all hypertension conditions using the SMOMED code for hypertension
     search = c.Condition.where(struct={'code': '38341003'})
     results = search.perform_resources(server)
     count = 0
     patients = []
     data = ''
-
+    #For each hypertension Condition resource returned, extract the corresponsing patient ID from the 'subject' field.
     for r in results:
         p_id = r.as_json()['subject']['reference'].split('/')[1]
         if p_id not in patients:
             patients.append(p_id)
-
+    #For each extracted patient ID, query for FHIR server for that Patient resource to extract their name.
     for p_id in patients:
         search = p.Patient.where(struct={'_id': p_id})
         results = search.perform_resources(server)
@@ -58,6 +61,6 @@ def getCondition():
                 count += 1
             except:
                 continue
-
+    #Return this information to app.py to include in the HTML rendering
     return 'ID and name of each patient with hypertension in the ' + settings['api_base'] + \
            ' FHIR server.\n\nTotal: ' + str(count) + '\n\n' + data
